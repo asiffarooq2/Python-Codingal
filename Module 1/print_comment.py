671842c3415754d5516ed77a09df496d3398b461
asiffarooq2 <asiffarooq2@gmail.com>
2026-06-22 09:40:47 +0530

    Initial

---
+++ Module 1/print_comment.py
@@ -0,0 +1,14 @@
+# This is a single-line comment
+
+print("Hello, World!")  # This prints a message
+
+# Printing multiple lines
+print("Welcome to Python")
+print("Learning is fun!")
+
+"""
+This is a multi-line comment.
+It can span multiple lines.
+"""
+
+print("Comments help explain code.")
